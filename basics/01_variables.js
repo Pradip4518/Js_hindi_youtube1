55d6cb1266c23c804085fd02000dab63e7821f87
Pradip Poshat <pradipposhat45@gmail.com>
2025-06-26 16:41:51 +0000

    DataTypes & Variables

---
+++ basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 22
+let accountEmail = "abc@gmail.com"
+var accountPassword = 9322
+accountCity = "Jaipur"
+
+//accountId = 55   not allowed
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
